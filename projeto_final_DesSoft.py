46eb98c2f87268bdad3cd3c01f60c23e175d1f0e
Hzfd <Hzfd@users.noreply.github.com>
2019-05-03 08:53:35 -0300

    avancei nos codigos

--- projeto_final_DesSoft.py
+++ projeto_final_DesSoft.py
@@ -29,4 +29,7 @@ YELLOW = (255, 255, 0)
 #criando o player 1
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        
+        pygame.sprite.Sprite.__init__(self)
+        player_img=pygame.image.load(path.join(img_dir,'#nomedaimagem')).convert()#colocar o nome da imagem do jogador
+        self.image=player_img
+        self.img=pygame.transform.scale((player_img(15,14)))
